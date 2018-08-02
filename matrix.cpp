b6cb0c4f4522ac5a4332cbe9b0f0cf0628ce038a
Aritra Sarkar <aritrasrkr1911@gmail.com>
2018-08-03 00:29:10 +0530

    Gauss-Jordan Elimination method converted from Julia to C (not tested).

--- matrix.cpp
+++ matrix.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 class Matrix {
 public:
-
     // Tip: Read a[i * cols + j] as a[i][j] just for ease of understanding
 
     void swap_rows(double * const a, size_t i, size_t pivot, size_t cols) {
@@ -62,54 +61,45 @@ public:
         }
     }
 
-    void back_substitution(const double * const a, double * const x,
+    void back_substitute(const double * const a, double * const x,
                            const size_t rows, const size_t cols) {
-        // x is the solution Vector
+        // Solve for solution vector x by back substitution
+        // *a must be in Row Echelon Form
 
-        for (int i = rows - 1; i >= 0; i--) {
+        for (size_t i = rows - 1; i >= 0; i--) {
             double sum = 0.0;
 
-            for (size_t j = cols - 2; j > i; j--) {
-                sum += x[j] * a[i * cols + j];
-            }
+            for (size_t j = cols - 2; j > i; j--)
+                sum += x[j] * a[i *cols+ j];
 
-            x[i] = (a[i * cols + cols - 1] - sum) / a[i * cols + i];
+            x[i] = (a[i *cols+ cols-1] - sum) / a[i *cols+ i];
         }
     }
 
-    // TODO: Covert this Julia into C.
-    //
     void reduce(double *a, const size_t rows, const size_t cols) {
-    //
-    //     # After this, we know what row to start on (r-1)
-    //     # to go back through the matrix
-    //     row = 1
-    //     for col = 1:cols-1
-    //         if (A[row, col] != 0)
-    //
-    //             # divide row by pivot and leaving pivot as 1
-    //             for i = cols:-1:col
-    //                 A[row,i] /= A[row,col]
-    //             end
-    //
-    //             # subtract value from above row and set values above pivot to 0
-    //             for i = 1:row-1
-    //                 for j = cols:-1:col
-    //                     A[i,j] -= A[i,col]*A[row,j]
-    //                 end
-    //             end
-    //             row += 1
-    //         end
-    //     end
+        // Applying Gauss-Jordan Elimination
+        // *a must be in Row Echelon Form
+
+        // After this, we know what row to start on (r-1)
+        // to go back through the matrix
+        pivot = 1
+        for(size_t k=1; k<cols; k++) {
+            if (a[pivot * cols + k] != 0) {
+
+                // divide row by pivot and leaving pivot as 1
+                for(size_t i=cols-1; i>=k; i--)
+                    a[pivot * cols + i] /= A[pivot * cols + k]
+
+                // subtract value from above row and set values above pivot to 0
+                for(size_t i=1; i<pivot; i++)
+                    for(size_t j=cols-1; j>=k; j--)
+                        a[i *cols+ j] -= a[i *cols+ k] * a[pivot *cols+ j]
+
+                pivot++
+            }
+        }
     }
 
-    void invert() { return; }
-
-    double determinant() { return 0.0; }
-
-    void adjoint() { return; }
-
-    void adjugate() { adjoint(); }
 };
 
 int main() {
@@ -129,7 +119,7 @@ int main() {
     cout << endl;
 
     double x[3] = {0, 0, 0};
-    Matrix().back_substitution((double *)a, x, 3, 4);
+    Matrix().back_substitute((double *)a, x, 3, 4);
 
     cout << "(" << x[0] << ", " << x[1] << ", " << x[2] << ")" << endl;
 
